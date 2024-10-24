d66cf293dd89f99983f2c6bf64a4d3a8547531ab
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-10-24 18:43:41 +0100

    Use GetMapping in WelcomeController

--- src/main/java/tn/esprit/tpfoyer17/controllers/WelcomeController.java
+++ src/main/java/tn/esprit/tpfoyer17/controllers/WelcomeController.java
@@ -1,17 +1,13 @@
 package tn.esprit.tpfoyer17.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class WelcomeController {
 
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
+    @GetMapping(value = "/")
     public String welcome() { return "From Docker Container: Bonjour, Bienvenue à l'application de test des Web Services REST."; }
 
 
