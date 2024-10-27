bbaa2671039afbcf3fb6eeb1be81a47304f4f0d2
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-10-28 00:55:01 +0100

    Update main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,17 +2,34 @@ provider "aws" {
   region = var.aws_region
 }
 
-
+#Network creation
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
 }
 
-# Create two subnets within the new VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
 resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "subnet1_association" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public_route_table.id
 }
 
 resource "aws_subnet" "subnet2" {
@@ -22,7 +39,12 @@ resource "aws_subnet" "subnet2" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_route_table_association" "subnet2_association" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
 
+#Cluster creation
 resource "aws_eks_cluster" "my_cluster" {
   name     = var.cluster_name
   role_arn = var.role_arn
